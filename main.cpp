ed5e4125ffca6ba9bb86c19a3f92c5f23dd80779
Isaac Medeiros <isaacoelhomedeiros@gmail.com>
2024-10-01 21:43:00 -0300

    criar uma janela em branco

--- main.cpp
+++ main.cpp
@@ -1,7 +1,59 @@
 #include <iostream>
+#include <SDL2/SDL.h>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+SDL_Window* gWindow = NULL;
+SDL_Surface* gSurface = NULL;
+
+bool init();
+bool loadMedia();
+void close();
 
 int main(int argc, char* args[]) {
-    printf("HELLO SDL");
+    init();    
 
     return 0;
+}
+
+bool init() {
+    bool success = true;
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("%s", SDL_GetError());
+        success = false;
+    } else {
+        gWindow = SDL_CreateWindow("Nome da janela", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+        if (gWindow == NULL) {
+            printf("%s", SDL_GetError());
+            success = false;
+        } else {
+            gSurface = SDL_GetWindowSurface(gWindow);
+
+            SDL_FillRect(gSurface, NULL, SDL_MapRGB(gSurface -> format, 0xFF, 0xFF, 0xFF));
+
+            SDL_UpdateWindowSurface(gWindow);
+
+            SDL_Event e; bool quit = false; while (quit == false) { while(SDL_PollEvent(&e)) { if(e.type == SDL_QUIT) quit = true; } } 
+
+            close();
+        }
+    }
+    
+
+    return success;
+}
+
+bool loadMedia() {
+    bool success = true;
+
+    return success;
+}
+
+void close() {
+    SDL_DestroyWindow(gWindow);
+
+    SDL_Quit();
 }
